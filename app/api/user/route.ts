b95b2a815f202bfcaf9892076fcef8ed823c5378
kimcy <01191318447@naver.com>
2023-10-24 13:32:09 +0900

    structure ing

---
+++ app/api/user/route.ts
@@ -0,0 +1,34 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+    const { email, nickname, password } = body;
+
+    const existingUserByEmail = await db.user.findUnique({
+      where: { email: email }
+    })
+
+    if (existingUserByEmail) {
+      return NextResponse.json({ user: null, message: '이미 가입된 이메일입니다.' }, { status: 409 })
+    }
+
+    const hashedPasswrd = await hash(password, 10)
+
+    const newUser = await db.user.create({
+      data: {
+        email,
+        password: hashedPasswrd,
+        nickname
+      }
+    })
+
+    return NextResponse.json({ user: newUser, message: '회원가입에 성공했습니다.' }, { status: 200 });
+  }
+  catch (error) {
+    return NextResponse.json({ message: '회원가입 중 문제가 발생하여 회원가입에 실패했습니다.' }, { status: 500 })
+  }
+
+}
--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -3,5 +3,7 @@ import * as z from 'zod';
 export const UserValidation = z.object({
   profile_photo: z.string(),
   name: z.string().min(2).max(20),
+  nickname: z.string().min(2).max(20),
+  email: z.string().min(10).max(50),
   bio: z.string(),
 })
